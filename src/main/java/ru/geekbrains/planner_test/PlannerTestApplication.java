180fb86bea8b7d5b8d9a36306bc07302921af4dc
MeSeurus <redoreole@gmail.com>
2022-06-27 20:07:00 +0300

    Initial Lesson 1 commit

---
+++ src/main/java/ru/geekbrains/planner_test/PlannerTestApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.planner_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/planner_test/PlannerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.planner_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
