abb19bc348598af39c0767111682aa10834ebeab
novitskynick <novitskynick@gmail.com>
2022-06-29 16:32:11 +0300

    some fixes

--- src/test/java/ru/geekbrains/planner_test/controllers/EventControllerTest.java
+++ src/test/java/ru/geekbrains/planner_test/controllers/EventControllerTest.java
@@ -121,9 +121,7 @@ public class EventControllerTest {
         mockMvc
                 .perform(
                         delete("/api/v1/event/1")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(mapper.writeValueAsString(buildEventDto()))
-                                .accept(MediaType.APPLICATION_JSON))
+                                .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
 
@@ -136,9 +134,7 @@ public class EventControllerTest {
         mockMvc
                 .perform(
                         delete("/api/v1/event/1")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(mapper.writeValueAsString(buildEventDto()))
-                                .accept(MediaType.APPLICATION_JSON))
+                                .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("message", is("Event with id = " + 1 + " not found for delete")));
