ba06728df5d719f0b960027690c66e6935e517fe
MeSeurus <redoreole@gmail.com>
2022-06-30 13:33:29 +0300

    Test commit for lesson 1. Main classes added. No config

---
+++ src/main/java/ru/geekbrains/planner_test/service/KanbanService.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.planner_test.service;
+
+import ru.geekbrains.planner_test.entity.KanbanEntity;
+import ru.geekbrains.planner_test.repository.KanbanRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KanbanService {
+
+    @Autowired
+    private KanbanRepository repository;
+
+    public List<KanbanEntity> findAll() {
+        var objects = (List<KanbanEntity>) repository.findAll();
+        return objects;
+    }
+
+}
