5ea3ac8217dd4c7c66a794e1e8bb31fe269df242
EVMihaylov <eugene.mikhailow@gmail.com>
2022-06-30 12:47:50 +0300

    - Разрешил конфликты перед merge

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,11 @@
+create table if not exists pets
+(id bigserial primary key, title varchar(100), score int);
+
+insert into pets(title, score)
+values  ('Dog', 100),
+        ('Cat', 100),
+        ('Mouse', 100);
+
 create table calendar_days
 (
     id      bigserial primary key,
